c955b0686575db1bb0df7ecf514b797e0017f616
KingWndr15 <abrahameyaife@gmail.com>
2024-06-07 12:20:14 +0100

    Done with implementing the auth pages

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Add all modified files to the staging area
 git add .
 
-# Prompt for a commit message
 read -p "Enter a commit message: " message
 
-# Commit with the provided message
 git commit -m "$message"
 
-# Push changes to the remote repository (replace 'origin' with your actual remote name)
-git push origin master
+git push orgin main
